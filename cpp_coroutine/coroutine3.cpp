b78cc4dc535af220d4150a87f87d4fdd92eca2c0
kenVS <yangyang280888@hotmail.com>
2024-03-21 10:11:34 +0800

    Add part 3: Log all method calls to sequence of coroutine

---
+++ cpp_coroutine/coroutine3.cpp
@@ -0,0 +1,85 @@
+/*
+* Logged all the methods to see sequence
+*/
+
+#include <iostream>
+#include <coroutine>
+
+std::coroutine_handle<> s_handle;
+bool s_first = true;
+
+struct Awaiter
+{
+    bool await_ready() const noexcept
+    {
+        std::cout << "[  awaiter] await_ready()\n";
+        return false;
+    }
+
+    void await_suspend(std::coroutine_handle<> handle)
+    {
+        std::cout << "[  awaiter] await_suspend()\n";
+        if (s_first)
+        {
+            std::cout << "[  awaiter] Assign coroutine handle\n";
+            s_handle = handle;
+            s_first = false;
+        }
+
+    }
+    void await_resume() const noexcept
+    {
+        std::cout << "[  awaiter] await_resume()\n";
+    }
+};
+
+struct Coro
+{
+    struct promise_type
+    {
+        Coro get_return_object()
+        {
+            std::cout << "[  promise] get_return_object()\n";
+            return {};
+        }
+        std::suspend_never initial_suspend()
+        {
+            std::cout << "[  promise] initial_suspend()\n";
+            return {};
+        }
+        std::suspend_never final_suspend() noexcept
+        {
+            std::cout << "[  promise] final_suspend()\n";
+            return {};
+        }
+        void unhandled_exception()
+        {
+            std::cout << "[  promise] unhandled_exception()\n";
+        }
+        void return_void()
+        {
+            std::cout << "[  promise] return_void()\n";
+        }
+    };
+};
+
+Coro MyCoroutine()
+{
+    std::cout << "[coroutine] Instruction 1\n";
+    co_await Awaiter();
+    std::cout << "[coroutine] Instruction 2\n";
+    co_await Awaiter();
+    std::cout << "[coroutine] Instruction 3\n";
+}
+
+int main()
+{
+    std::cout << "[     main] Invoke MyCoroutine()\n";
+    MyCoroutine();
+    std::cout << "[     main] Invoke coroutine handle 1\n";
+    s_handle();
+    std::cout << "[     main] Invoke coroutine handle 2\n";
+    s_handle();
+
+    std::cout << "[     main] End of main()\n";
+}
