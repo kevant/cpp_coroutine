e38af39b53e90b3b76dc64ff5ec527fc1c5f423d
ken <kenyang.tan@gmail.com>
2024-03-22 15:22:00 +0800

    Update step_by_step: Return coroutine_handle from coroutine return object

--- cpp_coroutine/step_by_step.cpp
+++ cpp_coroutine/step_by_step.cpp
@@ -1,52 +1,41 @@
 #include <iostream>
 #include <coroutine>
 
-std::coroutine_handle<> s_handle;
-bool s_first = true;
-
-struct Awaiter
-{
-    constexpr bool await_ready() const noexcept { return false; }
-    void await_suspend(std::coroutine_handle<> handle)
-    {
-        if (s_first)
-        {
-            s_handle = handle;
-            s_first = false;
-        }
-    }
-    constexpr void await_resume() const noexcept {}
-};
-
 struct Coro
 {
     struct promise_type
     {
-        Coro get_return_object() { return {}; }
+        Coro get_return_object() 
+        {
+            return { .handle = std::coroutine_handle<promise_type>::from_promise(*this) };
+        }
         std::suspend_never initial_suspend() { return {}; }
         std::suspend_never final_suspend() noexcept { return {}; }
         void unhandled_exception() {}
         void return_void() {}
     };
+
+    std::coroutine_handle<promise_type> handle;
 };
 
 Coro MyCoroutine()
 {
     std::cout << "[coroutine] Instruction 1\n";
-    co_await Awaiter();
+    co_await std::suspend_always();
     std::cout << "[coroutine] Instruction 2\n";
-    co_await Awaiter();
+    co_await std::suspend_always();
     std::cout << "[coroutine] Instruction 3\n";
 }
 
 int main()
 {
     std::cout << "[     main] Invoke MyCoroutine()\n";
-    MyCoroutine();
+    Coro coro = MyCoroutine();
+    std::coroutine_handle<> handle = coro.handle;
     std::cout << "[     main] Invoke coroutine handle\n";
-    s_handle();
+    handle();
     std::cout << "[     main] Invoke coroutine handle\n";
-    s_handle();
+    handle();
 
     std::cout << "[     main] End of main()\n";
 }
