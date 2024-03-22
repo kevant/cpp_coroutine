66f2f2afaef6678ba6f72bb73a57ff6c9003061d
ken <kenyang.tan@gmail.com>
2024-03-22 15:24:57 +0800

    Update step_by_step: co_yield

--- cpp_coroutine/step_by_step.cpp
+++ cpp_coroutine/step_by_step.cpp
@@ -5,14 +5,20 @@ struct Coro
 {
     struct promise_type
     {
+        int value = 0;
         Coro get_return_object() 
         {
             return { .handle = std::coroutine_handle<promise_type>::from_promise(*this) };
         }
-        std::suspend_never initial_suspend() { return {}; }
-        std::suspend_never final_suspend() noexcept { return {}; }
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
         void unhandled_exception() {}
         void return_void() {}
+        std::suspend_always yield_value(int v)
+        {
+            value = v;
+            return {};
+        }
     };
 
     std::coroutine_handle<promise_type> handle;
@@ -20,22 +26,29 @@ struct Coro
 
 Coro MyCoroutine()
 {
-    std::cout << "[coroutine] Instruction 1\n";
-    co_await std::suspend_always();
-    std::cout << "[coroutine] Instruction 2\n";
-    co_await std::suspend_always();
-    std::cout << "[coroutine] Instruction 3\n";
+    int i = 0;
+    while (true)
+    {
+        std::cout << "[coroutine] yield " << i << "\n";
+        co_yield i++;
+    }
 }
 
 int main()
 {
     std::cout << "[     main] Invoke MyCoroutine()\n";
     Coro coro = MyCoroutine();
-    std::coroutine_handle<> handle = coro.handle;
-    std::cout << "[     main] Invoke coroutine handle\n";
-    handle();
-    std::cout << "[     main] Invoke coroutine handle\n";
-    handle();
+    std::coroutine_handle<Coro::promise_type> handle = coro.handle;
+    Coro::promise_type& promise = handle.promise();
+    std::cout << "====================================\n";
 
+    for (int i = 0; i < 3; ++i)
+    {
+        handle();
+        std::cout << "[     main] promise.value: " << promise.value << '\n';
+    }
+
+    handle.destroy();
+    std::cout << "====================================\n";
     std::cout << "[     main] End of main()\n";
 }
