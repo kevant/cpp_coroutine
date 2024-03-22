5fd3f405737f038d46b88460ada5caeec9190a6c
ken <kenyang.tan@gmail.com>
2024-03-22 15:17:44 +0800

    Update step_by_step: Add Awaiter and coroutine_handle

--- cpp_coroutine/step_by_step.cpp
+++ cpp_coroutine/step_by_step.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <coroutine>
 
+std::coroutine_handle<> s_handle;
+bool s_first = true;
+
+struct Awaiter
+{
+    constexpr bool await_ready() const noexcept { return false; }
+    void await_suspend(std::coroutine_handle<> handle)
+    {
+        if (s_first)
+        {
+            s_handle = handle;
+            s_first = false;
+        }
+    }
+    constexpr void await_resume() const noexcept {}
+};
+
 struct Coro
 {
     struct promise_type
@@ -16,10 +33,9 @@ struct Coro
 Coro MyCoroutine()
 {
     std::cout << "[coroutine] Instruction 1\n";
-
-    co_await std::suspend_always();
-
+    co_await Awaiter();
     std::cout << "[coroutine] Instruction 2\n";
+    co_await Awaiter();
     std::cout << "[coroutine] Instruction 3\n";
 }
 
@@ -27,6 +43,10 @@ int main()
 {
     std::cout << "[     main] Invoke MyCoroutine()\n";
     MyCoroutine();
+    std::cout << "[     main] Invoke coroutine handle\n";
+    s_handle();
+    std::cout << "[     main] Invoke coroutine handle\n";
+    s_handle();
 
     std::cout << "[     main] End of main()\n";
 }
