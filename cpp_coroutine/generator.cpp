a3935022925e1a2c21a8200c9ff88c663d471630
kenyang <kenyang.tan@gmail.com>
2024-03-28 16:37:45 +0800

    Update generator code for clarity

--- cpp_coroutine/generator.cpp
+++ cpp_coroutine/generator.cpp
@@ -12,9 +12,11 @@ struct Generator
         {
             return { .handle = std::coroutine_handle<promise_type>::from_promise(*this) };
         }
-        std::suspend_always initial_suspend() { return {}; }
+        // don't suspend for initial_suspend, so the first yield value will be return
+        // can consider suspending initial_suspend if the calculation is expensive
+        std::suspend_never initial_suspend() { return {}; }
         std::suspend_always final_suspend() noexcept { return {}; }
-        void unhandled_exception(){}
+        void unhandled_exception() {}
         std::suspend_always yield_value(T v)
         {
             value = v;
@@ -25,18 +27,24 @@ struct Generator
 
     std::coroutine_handle<promise_type> handle;
 
-    T operator()() 
+    T get_value()
     {
-        handle();
         return handle.promise().value;
     }
-};
 
+    void resume()
+    {
+        // the check is performed to ensure that
+        // handle will not be invoked if handle.done() == true
+        if (!handle.done())
+            handle.resume();
+    }
+};
 
 Generator<int> MyGenerator()
 {
     int i = 0;
-    while(true)
+    while (true)
         co_yield i++;
 }
 
@@ -60,14 +68,16 @@ int main()
     auto gen = MyGenerator();
     for (int i = 0; i < 3; ++i)
     {
-        std::cout << "generator value: " << gen() << '\n';
+        std::cout << "generator value: " << gen.get_value() << '\n';
+        gen.resume();
     }
     std::cout << "====================================\n";
 
     auto fib_gen = FibGenerator();
     for (int i = 0; i < 10; ++i)
     {
-        std::cout << "FibGenerator value: " << fib_gen() << '\n';
+        std::cout << "generator value: " << fib_gen.get_value() << '\n';
+        fib_gen.resume();
     }
     std::cout << "====================================\n";
 }
